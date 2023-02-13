b55f2ae4eb90021357cf60da18ba3b9eb2d29067
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2023-02-13 12:57:31 +0000

    secondo_commit

---
+++ templates/esercizio.py
@@ -0,0 +1,5 @@
+#realizzare un server web con Flask che offra le seguenti funzionalità:
+#1) la home page deve fornire una breve descrizione delle caratteristiche della città di milano
+#2) al link <nomesito>/immagini devono essere visualizzate le immagini di 4 monumenti di milano (controllare sul sito del prof come si fa ad inserire le immagini)
+#3) fare in modo che liccando su un'immagine venga mandati ad un breve testo descrittivo del monumento ( controllare sempre sul sito del docente)
+#la repository che conterrà il sito si chiamerà FlaskMilano
